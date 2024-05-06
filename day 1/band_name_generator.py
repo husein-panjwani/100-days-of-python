8fffeacb8d80a584849bb0432da9346d9f4f621a
mohamed husein panjwani <91213265+husein-panjwani@users.noreply.github.com>
2024-05-06 15:14:38 +0530

    Update band_name_generator.py

--- day 1/band_name_generator.py
+++ day 1/band_name_generator.py
@@ -1,1 +1,7 @@
+print("welcome to band generator")
+
+city=input("what is the name of city u were born?\n")
+pet=input("what is the name of your pet?\n")
+
+print("your band name is " + city +" "+ pet)
 
