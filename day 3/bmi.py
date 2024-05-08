08e296579f10ad2436e5849c2c47b2fe40c5a530
mohamed husein panjwani <91213265+husein-panjwani@users.noreply.github.com>
2024-05-08 16:07:36 +0530

    Update bmi.py

--- day 3/bmi.py
+++ day 3/bmi.py
@@ -1,17 +1,1 @@
-h=input("enter height in m: ")
-w=input("enter weight in kg: ")
 
-h = float(h)
-w = float(w)
-bmi= w/(h**2)
-bmi = round(bmi,2)
-if bmi <= 18.5:
-    print(f"your bmi is {bmi}, you are underweight")
-elif bmi >18.5 and bmi<=25:
-    print(f"your bmi is {bmi}, you are normal")
-elif bmi> 25 and bmi<=30:
-    print(f"your bmi is {bmi}, you are overweight")
-elif bmi > 30 and bmi<=35:
-    print(f"your bmi is {bmi}, you are obese")
-elif bmi > 35 :
-    print(f"your bmi is {bmi}, you are clinically obese")
